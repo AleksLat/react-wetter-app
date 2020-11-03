3189546d408c7362bcde86817f3ee6ee08574924
AleksLat <66917008+AleksLat@users.noreply.github.com>
2020-11-03 11:42:08 +0100

    Added the links GitH and Personal

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ export default function App() {
   return (
     <div className="App">
      Weather App
+     <footer>
+       This project was coded by 
+       <a href="https://www.linkedin.com/in/aleks-lat/" 
+       target="_blank">
+       Aleks Lat 
+       </a>
+       and is 
+     <a href="https://github.com/AleksLat/react-wetter-app"
+     target="_blank">
+       open-sourced on GitHub
+     </a>
+     </footer>
+     
     </div>
   );
 }
