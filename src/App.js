76aa79060eedba3e3548ee70947f93660d150cbf
AleksLat <66917008+AleksLat@users.noreply.github.com>
2020-11-03 16:08:23 +0100

    Added spaces

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ export default function App() {
     <div className="App">
      Weather App
      <footer>
-       This project was coded by 
+       This project was coded by{" "}
        <a href="https://www.linkedin.com/in/aleks-lat/" 
        target="_blank">
        Aleks Lat 
-       </a>
-       and is 
+       </a>{" "}
+        and is {" "}
      <a href="https://github.com/AleksLat/react-wetter-app"
      target="_blank">
        open-sourced on GitHub
