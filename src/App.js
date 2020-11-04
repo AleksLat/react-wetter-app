f7ffe5e064f5fa16a8d10a4ad01c6d2eb2bdbecc
AleksLat <66917008+AleksLat@users.noreply.github.com>
2020-11-04 09:58:49 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     Weather App
-     <footer>
+      <div className="container">
+      Weather App
+        <footer>
        This project was coded by{" "}
-       <a href="https://www.linkedin.com/in/aleks-lat/" 
+        <a href="https://www.linkedin.com/in/aleks-lat/" 
        target="_blank">
        Aleks Lat 
-       </a>{" "}
+         </a>{" "}
         and is {" "}
-     <a href="https://github.com/AleksLat/react-wetter-app"
+         <a href="https://github.com/AleksLat/react-wetter-app"
      target="_blank">
        open-sourced on GitHub
-     </a>
-     </footer>
-     
+         </a>
+       </footer>
+      </div>
     </div>
   );
 }
